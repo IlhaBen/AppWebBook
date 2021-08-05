592f7b8cb8520bf2111be19633f5617593350179
Ilham Ilham <ben.ilhamm@gmail.com>
2021-08-05 17:52:41 -0300

    Ajoutez des fichiers projet.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using AppWebBook.Models;
+
+namespace AppWebBook.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<AppWebBook.Models.Book> Book { get; set; }
+    }
+}
